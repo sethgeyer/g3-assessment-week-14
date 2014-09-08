86c786dfb99ee9b39a989d56ce5a744a1ce429fd
Seth Geyer <seth.geyer@gmail.com>
2014-09-08 11:48:25 -0600

    committed final

--- app/models/prescription.rb
+++ app/models/prescription.rb
@@ -12,13 +12,13 @@ class Prescription < ActiveRecord::Base
   validate :dosage_not_a_number
 
   def end_date_after_start_date
-    if self[:end_date] < self[:start_date]
+    if self[:end_date] == nil ||  self[:end_date] < self[:start_date]
     errors[:end_date] << "must be after start date"
     end
   end
 
   def dosage_not_a_number
-    if self[:dosage][0].to_i = 0
+    if self[:dosage] == "" || self[:dosage][0].to_i = 0
       errors[:dosage] << "must start with a number"
     end
   end
